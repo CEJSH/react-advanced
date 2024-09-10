e351ce5d06a74a3218f0233c3cf149e02a4f96e6
CEJSH <eun2763@naver.com>
2024-09-10 23:00:53 +0900

    feat: Add ImageGallery

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -1,5 +1,22 @@
+import classNames from 'classnames'
 import Section from '@shared/Section'
+import styles from './Video.module.scss'
+
+const cx = classNames.bind(styles)
 
 export default function Video() {
-  return <Section>Video</Section>
+  return (
+    <Section className={cx('container')}>
+      <video
+        controls
+        autoPlay={true}
+        src="/assets/main.mp4"
+        width={'100%'}
+        loop={true}
+        poster="/assets/poster.jpeg"
+      >
+        {/* <source src="/assets/main.mp4" type="video/mp4" /> */}
+      </video>
+    </Section>
+  )
 }
